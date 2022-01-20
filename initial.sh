401de49140e4bbe3aabf5636ea528260b0777975
apkawa <apkawa@gmail.com>
2022-01-20 16:46:31 +0300

    Fixes for CI

--- initial.sh
+++ initial.sh
@@ -6,7 +6,7 @@ PACKAGE_NAME=$2
 
 git reset $(git rev-list --max-parents=0 --abbrev-commit HEAD)
 find . -type f -exec sed -i -e "s/__python-library-template__/$PROJECT_NAME/g" -e "s/__example_library__/$PACKAGE_NAME/g" {} \;
-mv example_library $PACKAGE_NAME
+mv __example_library__ $PACKAGE_NAME
 rm -f $0
 
 git add .
